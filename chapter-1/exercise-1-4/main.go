71fe6b4b475d1ace1d49bed2f5bef01faa34e6fe
Bradley Bossard <bradleybossard@gmail.com>
2018-09-07 14:26:03 -0500

    Add exercise descriptions for 1-4, 1-5, 1-6

---
+++ chapter-1/exercise-1-4/main.go
@@ -0,0 +1,11 @@
+/*
+Exercise 1.4: Modify dup2 to print the names of all files in which each
+duplicated line occurs.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
---
+++ chapter-1/exercise-1-6/main.go
@@ -0,0 +1,12 @@
+/*
+Exercise 1.6: Modify the Lissajous program to produce images in multiple colors
+by adding more values to palette and then displaying them by changing the
+third argument of Set ColorIndex in some interesting way.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
