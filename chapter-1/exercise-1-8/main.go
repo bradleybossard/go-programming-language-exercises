b88b9c8395b15db6a9f007cde9cd451458d32617
Bradley Bossard <bradleybossard@gmail.com>
2018-09-07 14:49:42 -0500

    Add exercises 1-7, 1-8, 1-9

---
+++ chapter-1/exercise-1-8/main.go
@@ -0,0 +1,12 @@
+/*
+Exercise 1.8: Modify fetch to add the prefix http:// to each argument URL if it
+is missing. You might want to use strings.HasPrefix.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
---
+++ chapter-1/exercise-1-9/main.go
@@ -0,0 +1,11 @@
+/*
+Exercise 1.9: Modify fetch to also print the HTTP status code,
+found in resp.Status.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
