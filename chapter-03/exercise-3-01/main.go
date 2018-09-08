d74f91f84c1db2bf3241e42aacc5df691dffcfa2
Bradley Bossard <bradleybossard@gmail.com>
2018-09-07 19:14:54 -0500

    Add chapter 3, 1-4

---
+++ chapter-03/exercise-3-01/main.go
@@ -0,0 +1,13 @@
+/*
+Exercise 3.1: If the function f returns a non-finite float64 value, the SVG
+file will contain invalid <polygon> elements (although many SVG renderers hand
+let his gracefully). Modify the program to skip invalid polygons.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
---
+++ chapter-03/exercise-3-02/main.go
@@ -0,0 +1,11 @@
+/*
+Exercise 3.2: Experiment with visualizations of other functions from the math
+package. Can you produce an egg box, moguls, or a saddle?
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
