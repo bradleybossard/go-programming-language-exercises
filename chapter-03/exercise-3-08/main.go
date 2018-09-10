c11a95c6c92b86f047d016a282d04e91278b9053
Bradley Bossard <bradleybossard@gmail.com>
2018-09-10 13:17:23 -0500

    Add exercises 3-08 - 3-10

---
+++ chapter-03/exercise-3-08/main.go
@@ -0,0 +1,16 @@
+/*
+Exercise 3.8: Rendering fractals at high zoom levels demands great arithmetic
+precision. Implement the same fractal using four different represent at ions of
+numbers: complex64, complex128, big.Float, and big.Rat. (The latter two types
+are found in the math/big package. Float uses arbitrary but bounded-precision
+floating-point; Rat uses unbounded-precision rational numbers.) How do they
+compare in performance and memory usage? At what zoom levels do rendering
+artifacts become visible?
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
---
+++ chapter-03/exercise-3-10/main.go
@@ -0,0 +1,11 @@
+/*
+Exercise 3.10: Write a non-recursive version of comma, using bytes.Buffer
+instead of string concatenation.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
